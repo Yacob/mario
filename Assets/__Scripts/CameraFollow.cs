c43d38195bf15ca1bcad4af3c36faf9df4d75385
Luke Anderson <lithas@umich.edu>
2014-01-25 21:10:36 -0500

    Changes camera file

--- Assets/__Scripts/CameraFollow.cs
+++ Assets/__Scripts/CameraFollow.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 	public Transform 	poi; // Point of Interest
 	public float		u;
+	public float		y = 4;
 	public Vector3		offset = new Vector3(0,0,-5);
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 poiV3 = poi.position + offset;
 		Vector3 currPos = transform.position;
-		Vector3 pos = (1-u)*currPos + u*poiV3;
-		transform.position = pos;
+		if (currPos.x <= poiV3.x) {
+			Vector3 pos = (1 - u) * currPos + u * poiV3;
+			pos.y = 4;
+			transform.position = pos;
+		}
 
 		//if y position is < some value we raise camera so that the ground is bottom of screen
 		//same thing for the ceiling
