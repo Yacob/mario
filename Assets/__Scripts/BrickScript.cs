6e295fdbe15516e43adefb85dda959498a1afe73
Luke Anderson <lithas@umich.edu>
2014-01-27 21:31:54 -0500

    changed bricks to use prefab

---
+++ Assets/__Scripts/BrickScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickScript : MonoBehaviour {
+
+	public int 			initialCoins; // Point of Interest
+	public bool			hasCoins;
+	public bool			has1Up;
+	public bool			hasStar;
+	public bool			hasMushroom;
+	public bool			isHidden;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/__Scripts/coinBoxHit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinBoxHit : MonoBehaviour {
+
+	public int 			initialCoins; // Point of Interest
+	public bool			hasCoins;
+	public bool			has1Up;
+	public bool			hasStar;
+	public bool			hasMushroom;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
