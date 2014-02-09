e54f3e28ed594532d7cd440d9674bd6965470ca4
Jacob Nestor <nestor.jac@gmail.com>
2014-02-08 22:19:26 -0500

    music

--- Assets/__Scripts/FallenToDeath.cs
+++ Assets/__Scripts/FallenToDeath.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class FallenToDeath : MonoBehaviour {
 
-	void OnTriggerEnter(){
-		if (Mario.lives <= 1) {
-			Mario.dead = true;
-			Mario.respawn = false;
-		}
-		else {
-			Debug.Log("dead?");
-			Mario.respawn = true;
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			if (Mario.lives <= 1) {
+				Mario.dead = true;
+				Mario.respawn = false;
+			} 
+			else {
+				Debug.Log ("dead?");
+				Mario.respawn = true;
+			}
 		}
 	}
-	
 
 }
